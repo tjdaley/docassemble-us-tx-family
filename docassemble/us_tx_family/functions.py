1745c432771e44870c9d2c6f76ab08269c1a3a28
Tom Daley <tjd@powerdaley.com>
2019-12-27 00:19:09 -0600

    use counties()

---
+++ docassemble/us_tx_family/functions.py
@@ -0,0 +1,11 @@
+"""
+functions.py - Functions for docassemble interviews.
+
+Copyright (c) 2019 by Thomas J. Daley, J.D. All Rights Reserved.
+"""
+from .us_tx_counties import UsTxCounties
+
+
+def counties():
+    my_counties = UsTxCounties()
+    return my_counties.get_counties
