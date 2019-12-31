130556179871c9d7b615940d3d41661d2c5c2752
Tom Daley <tjd@powerdaley.com>
2019-12-30 19:45:59 -0600

    might finally be on to something

--- docassemble/us_tx_family/objects.py
+++ docassemble/us_tx_family/objects.py
@@ -39,14 +39,10 @@ class Job(DAObject):
     income taxes.
     """
     def init(self, *pargs, **kwargs):
-        #if 'self_employed' not in kwargs:
-        #    self.self_employed = False
-        #if 'employer' not in kwargs:
-        #    self.employer = None
-        #if 'income' not in kwargs:
-        self.initializeAttribute('income', MyPeriodicValue)
-        #if 'union_dues' not in kwargs:
-        self.initializeAttribute('union_dues', MyPeriodicValue)
+        if 'income' not in kwargs:
+            self.initializeAttribute('income', MyPeriodicValue)
+        if 'union_dues' not in kwargs:
+            self.initializeAttribute('union_dues', MyPeriodicValue)
         super(Job, self).init(*pargs, **kwargs)
 
     def summary(self):
@@ -55,9 +51,6 @@ class Job(DAObject):
     @property
     def complete(self):
         return self.employer is not None
-        #self.self_employed
-        #self.income
-        #self.union_dues
 
     def __unicode__(self):
         return self.summary()
@@ -66,7 +59,7 @@ class Job(DAObject):
 class IncomeList(DAList):
     def init(self, *pargs, **kwargs):
         self.object_type = Income
-        self.complete_attribute = 'income_complete'
+        self.complete_attribute = 'complete'
         super(IncomeList, self).init(*pargs, **kwargs)
 
 
@@ -77,25 +70,16 @@ class Income(DAObject):
     subject to income taxes.
     """
     def init(self, *pargs, **kwargs):
-        if 'description' not in kwargs:
-            self.description = None
         if 'income' not in kwargs:
             self.initializeAttribute('income', MyPeriodicValue)
-        if 'is_taxable' not in kwargs:
-            self.is_taxable = False
-        if 'type' not in kwargs:
-            self.type = None
         super(Income, self).init(*pargs, **kwargs)
 
     def summary(self):
         return self.description or "**NONE**"
 
     @property
-    def income_complete(self):
-        self.description
-        self.income
-        self.is_taxable
-        self.type
+    def complete(self):
+        return self.description is not None
 
     def __unicode__(self):
         return self.summary()
