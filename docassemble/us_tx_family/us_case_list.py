67e4396b2b098ac1d4c8352faacd58642afb402d
tjdaley <tjd@powerdaley.com>
2020-01-05 22:02:35 -0600

    enhance logging

--- docassemble/us_tx_family/us_case_list.py
+++ docassemble/us_tx_family/us_case_list.py
@@ -18,9 +18,9 @@ if not DEV_MODE:
 else:
     def logmessage(message: str):
         try:
-            sys.stderr.write(message + '\n')
+            sys.stderr.write("@@@@@ " + message + '\n')
         except Exception as e:
-            sys.stderr.write('Unable to log: {}'.format(str(e)))
+            sys.stderr.write('@@@@@ Unable to log: {}'.format(str(e)))
 
 STORE = '{}:us_case_list'
 
@@ -44,40 +44,40 @@ class UsCaseList(object):
         self.cases = self.read() or {}
 
     def get_cases(self):
-        case_list = [(key, case) for key, case in self.cases.items()]
-        logmessage(str(case_list))
+        case_list = [(key, case) for key, case in self.cases.items() if hasattr(case, 'case_id')]
+        logmessage("get_cases(): " + str(case_list))
         return case_list
 
     def get_case(self, key: str):
-        logmessage("Found {} cases to look through.".format(len(self.cases)))
-        logmessage("Searching for {}'s case, key={}".format(self.user_id, key))
+        logmessage("get_case(): " + "Found {} cases to look through.".format(len(self.cases)))
+        logmessage("get_case(): " + "Searching for {}'s case, key={}".format(self.user_id, key))
         case = self.cases.get(key)
-        logmessage("Retrieved case {}".format(str(case)))
+        logmessage("get_case(): " + "Retrieved case {}".format(str(case)))
         return case
 
     def del_case(self, key: str):
-        logmessage("Trying to delete case, key={}".format(key))
+        logmessage("del_case(): " + "Trying to delete case, key={}".format(key))
         if key in self.cases:
             del self.cases[key]
             self.save(None)
         else:
-            logmessage("Could not find case having key={}".format(key))
+            logmessage("del_case(): " + "Could not find case having key={}".format(key))
 
     def del_cases(self):
         if not DEV_MODE:
-            logmessage("Deleting all cases for user = {}".format(self.user_id))
+            logmessage("del_cases(): " + "Deleting all cases for user = {}".format(self.user_id))
             the_redis = DARedis()
             the_redis.set_data(self.store, None)
             return
 
-        logmessage("Cannot delete all cases in DEV_MODE")
+        logmessage("del_case(): " + "Cannot delete all cases in DEV_MODE")
 
     def read(self) -> dict:
         if DEV_MODE:
             cases = self.dev_read()
         else:
             cases = self.prod_read()
-        logmessage("Retrieved {} cases from {}.".format(len(cases or []), self.store))
+        logmessage("read(): " + "Retrieved {} cases from {}.".format(len(cases or []), self.store))
         return cases
 
     def save(self, case) -> bool:
