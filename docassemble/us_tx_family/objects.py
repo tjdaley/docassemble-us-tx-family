ba49b575b18dfff3a965039d99f3da70f6490db5
Tom Daley <tjd@powerdaley.com>
2020-01-14 02:18:41 -0600

    fix error in import list

--- docassemble/us_tx_family/objects.py
+++ docassemble/us_tx_family/objects.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 by Thomas J. Daley, J.D. All Rights Reserved.
 """
 from decimal import Decimal
 
-from docassemble.base.util import Address DAList, DAObject, Individual,  PeriodicValue, Person, word
+from docassemble.base.util import Address, DAList, DAObject, Individual,  PeriodicValue, Person, word
 
 __all__ = ['Attorney', 'Job', 'JobList', 'Income', 'IncomeList', 'MyPeriodicValue']
 
