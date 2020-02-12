acdcf38185a0b2585947d8ea10be3d6c1db5afee
Tom Daley <tjd@powerdaley.com>
2020-02-12 06:27:41 -0600

    modify for development use

--- docassemble/us_tx_family/local_config.py
+++ docassemble/us_tx_family/local_config.py
@@ -4,12 +4,31 @@ package.
 
 Copyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
 """
-from docassemble.base.functions import get_config
+try:
+    from docassemble.base.functions import get_config
+except Exception:
+    # In develpment, we don't have the docassemble api installed.
+    # This code mimics the docassemble get_config() function
+    # for testing on my local workstation. TJD 2020-02-11
+    import os
+
+    class EnvironConfig(object):
+        def get(self, param_name: str, default) -> str:
+            lookup_name = param_name.replace(' ', '')
+            if param_name in os.environ:
+                return os.environ[lookup_name]
+            print(f"get_config(): Environment variable '{lookup_name}' not found. Returning default of '{default}'")
+            return default
+
+    def get_config(environment):
+        # environment is ignored
+        config = EnvironConfig()
+        return config
 
 __all__ = ['local_config']
 
 
-def local_config(param_name: str, default, dev_mode: bool = False):
+def local_config(param_name: str, default=None, dev_mode: bool = False):
     if dev_mode:
         return default
     config = get_config('us-tx-family')
